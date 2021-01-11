83b02c5d49cbe39a9e276242ccb9a5e7b0ca682f
mariomui <yi.kan.mui@gmail.com>
2021-01-11 05:02:16 -0800

    Write some documentation on exactly how lifecyles work and how to give time for sideEffects to render renders

--- masterlist/components/layouts/ImgSource.js
+++ masterlist/components/layouts/ImgSource.js
@@ -21,20 +21,38 @@ export const ImgSource = ({ primaryImg, secondaryImg }) => {
   const imgRef = useRef(null);
   const [isImgWithinView, setIsImgWithinView] = useState(false);
 
+  // handles hiding our img before it is ready to load.
+  const [isLoading, setIsLoading] = useState(true);
+
   const setPrimaryImg = () => {
     imgRef.current.src = primaryImg;
   };
   const setSecondaryImg = () => {
     imgRef.current.src = secondaryImg;
   };
-  const handleScroll = useCallback(() => {
+
+  const checkIfImgInView = useCallback(() => {
     const { top, bottom } = imgRef.current.getBoundingClientRect();
     const isTopOfImgNorthOfWindow = top < 0;
     const isBottomOfImgSouthOfWindow = bottom > window.innerHeight;
-    const condition = !isTopOfImgNorthOfWindow && !isBottomOfImgSouthOfWindow;
+    return !isTopOfImgNorthOfWindow && !isBottomOfImgSouthOfWindow;
+  }, []);
+  const handleScroll = useCallback(() => {
+    const condition = checkIfImgInView();
     setIsImgWithinView(condition);
   }, []);
+
   useEffect(() => {
+    // after render, show our image.
+    // this allows time for the useEffect logic to either gray or color the image on load.
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    // handle the toggle on mount (after render);
+    setIsImgWithinView(checkIfImgInView());
+
+    // handles the toggle on Scroll
     window.addEventListener('scroll', handleScroll);
     return function cleanup() {
       window.removeEventListener('scroll', handleScroll);
@@ -43,7 +61,13 @@ export const ImgSource = ({ primaryImg, secondaryImg }) => {
   return (
     <SImgSquare>
       <img
-        src={!isImgWithinView ? secondaryImg : primaryImg}
+        src={
+          isLoading
+            ? 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==' // 1x1gif
+            : !isImgWithinView
+            ? secondaryImg
+            : primaryImg
+        }
         ref={imgRef}
         onMouseOver={setPrimaryImg}
         // onMouseOut={setSecondaryImg}
