708073d68745af8ca57cf78e58b01f5cbef4a222
mariomui <yi.kan.mui@gmail.com>
2021-01-10 22:02:11 -0800

    create a debounced function that will only generale a listening after stopped typing for a while.

--- masterlist/pages/index.js
+++ masterlist/pages/index.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import styled from '@emotion/styled';
+import { debounce } from '../core/helper';
 
 /*
-i want to use smart components when 
 A Smart Component is any component which manages its own state. 
 Any Dumb Component will have a S prefixed for styled.
 */
-const InputElement = () => {
-  return (
-    <input placeholder="Enter some text" name="example" type="text"></input>
-  );
-};
+const SInputElement = styled.input`
+  font-size: 1rem;
+  color: green;
+`;
+
 const LandingPage = () => {
+  // i have to debounce the handler.
+  const [inputText, setInputText] = useState('');
+  const debouncedSearch = useCallback(
+    debounce((value) => {
+      console.log(value, 'saving');
+    }, 1200),
+    []
+  );
+  const handleOnInputChange = (e) => {
+    setInputText(e.target.value);
+    debouncedSearch(e.target.value);
+  };
   return (
     <>
-      <InputElement />
+      <SInputElement
+        onChange={handleOnInputChange}
+        placeholder="Enter some text"
+        name="example"
+        type="text"
+        value={inputText}
+      />
     </>
   );
 };
